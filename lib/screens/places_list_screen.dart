f24efe0d411246566f2c1cf91f7e9829bafb6423
a7md0 <a7m3d699@gmail.com>
2021-03-02 18:38:48 +0300

    places list sc render places list

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:great_places_app/providers/great_places.dart';
 import 'package:great_places_app/screens/add_place_screen.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
   const PlacesListScreen({Key key}) : super(key: key);
@@ -18,8 +20,28 @@ class PlacesListScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Center(
-        child: CircularProgressIndicator(),
+      body: Consumer<GreatPlaces>(
+        child: Center(
+          child: const Text('No places yet, start adding some!'),
+        ),
+        builder: (ctx, greatPlaces, child) {
+          if (greatPlaces.items.length == 0) {
+            return child;
+          }
+
+          return ListView.builder(
+            itemCount: greatPlaces.items.length,
+            itemBuilder: (ctx, idx) => ListTile(
+              leading: CircleAvatar(
+                backgroundImage: FileImage(greatPlaces.items[idx].image),
+              ),
+              title: Text(greatPlaces.items[idx].title),
+              onTap: () {
+                // TODO: Details page
+              },
+            ),
+          );
+        },
       ),
     );
   }
