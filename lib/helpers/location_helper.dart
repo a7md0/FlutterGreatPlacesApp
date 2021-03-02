9e1d868088a7ce8e6f5857190980ca055f581769
a7md0 <a7m3d699@gmail.com>
2021-03-03 00:31:14 +0300

    GMaps change label to A

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -5,6 +5,6 @@ class LocationHelper {
     double latitude,
     double longitude,
   }) {
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7C$latitude,$longitude&key=$GOOGLE_MAP_API_KEY';
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_MAP_API_KEY';
   }
 }
