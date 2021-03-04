dbf674558f4c4ee086054f89774f9436f5ff5b57
a7md0 <a7m3d699@gmail.com>
2021-03-05 01:56:51 +0300

    loc helper api for street address

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
+
 const GOOGLE_MAP_API_KEY = 'AIzaSyCTqQnXecAiNXwpfGvk2i-g5BSwozjPI_c';
 
 class LocationHelper {
@@ -6,5 +11,14 @@ class LocationHelper {
     double longitude,
   }) {
     return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_MAP_API_KEY';
+  }
+
+  static Future<String> getPlaceAddress(double lat, double lng) async {
+    final url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$GOOGLE_MAP_API_KEY';
+
+    final response = await http.get(url);
+    final body = json.decode(response.body);
+
+    return body['results'][0]['formatted_address'];
   }
 }
